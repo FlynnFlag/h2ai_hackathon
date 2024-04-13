87f799105d9b73da186f7ef556847f5ba2f4338b
Lianghui Yi <ly297@georgetown.edu>
2024-04-13 17:52:29 -0400

    update agent def

--- lambda_functions/package/lambda_function.py
+++ lambda_functions/package/lambda_function.py
@@ -1,10 +1,12 @@
 import json
 import requests
     
-def search_hospital_nearby(place_name):
+def search_doctor_nearby(place_name):
     # search the doctors based on the location and return doctor names, rate and their location
+    import requests
     api_key = 'AIzaSyCLVC_U5LIlNCbSINkahciTmO4kkf63gIs'
     url = f'https://maps.googleapis.com/maps/api/geocode/json?address={place_name}&key={api_key}'
+    detail_url = 'https://maps.googleapis.com/maps/api/place/details/'
     try:
         response = requests.get(url)
         data = response.json()
@@ -19,13 +21,34 @@ def search_hospital_nearby(place_name):
             'location': str(latitude)+","+str(longitude),
             'radius': 5000,
             'type': "doctor",
-            'keyword': 'pulmonologists'
+            'keyword': 'pulmonologists',
+            "rankby" : "prominence"
         }
         response = requests.get(url, params=params)
-        results = response.json()
-        res = []
-        for result in results['results']:
-            res.append(result['name'])
+        results = response.json()['results'][:5]
+
+        place_ids = []
+        
+        for result in results:
+            place_ids.append(result['place_id'])
+
+        res = []        
+        for place_id in place_ids:
+            place_id_url = f'{detail_url}json?place_id={place_id}&key={api_key}'
+            details = requests.get(f'{detail_url}json?place_id={place_id}&key={api_key}').json()['result']
+            item = {}
+            item['name'] = details.get('name')
+            item["address"] = details.get('formatted_address')
+            item["phone"] = details.get('formatted_phone_number')
+            item["website"] = details.get('website')
+            item['rating'] = details.get("rating")
+            try:
+                item["top_review"] = details.get("reviews")[0]['text']
+            except:
+                item["top_review"] = None
+            item["wheelchair_accessible_entrance"] = details.get('wheelchair_accessible_entrance')
+            res.append(item)
+
         return res
     except:
         return "no relevant results"
@@ -34,7 +57,7 @@ def lambda_handler(event, context):
     # Extract the place name from the event
     place_name = event['queryStringParameters']['place_name']
     # Call the search_hospital_nearby function
-    results = search_hospital_nearby(place_name)
+    results = search_doctor_nearby(place_name)
     # Return the results
     return {
         'statusCode': 200,
